13f46172bbdea1123bdde4f22259c770b9a92597
touchthesky80 <touchthesky80@gmail.com>
2021-06-05 23:12:49 +0900

    Testcase 작성하기 쉽게 수정

---
+++ GildedRose/src/GildedRose.Console/GildedRose.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRose.Console
+{
+    public class GildedRose
+    {
+        public IList<Item> items;
+
+        public GildedRose(IList<Item> items)
+        {
+            this.items = items;
+        }
+
+        public void UpdateQuality()
+        {            
+            for (var i = 0; i < items.Count; i++)
+            {
+                if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (items[i].quality > 0)
+                    {
+                        if (items[i].name != "Sulfuras, Hand of Ragnaros")
+                        {
+                            items[i].quality = items[i].quality - 1;
+                        }
+                    }
+                }
+                else
+                {
+                    if (items[i].quality < 50)
+                    {
+                        items[i].quality = items[i].quality + 1;
+
+                        if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (items[i].sellIn < 11)
+                            {
+                                if (items[i].quality < 50)
+                                {
+                                    items[i].quality = items[i].quality + 1;
+                                }
+                            }
+
+                            if (items[i].sellIn < 6)
+                            {
+                                if (items[i].quality < 50)
+                                {
+                                    items[i].quality = items[i].quality + 1;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (items[i].name != "Sulfuras, Hand of Ragnaros")
+                {
+                    items[i].sellIn = items[i].sellIn - 1;
+                }
+
+                if (items[i].sellIn < 0)
+                {
+                    if (items[i].name != "Aged Brie")
+                    {
+                        if (items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (items[i].quality > 0)
+                            {
+                                if (items[i].name != "Sulfuras, Hand of Ragnaros")
+                                {
+                                    items[i].quality = items[i].quality - 1;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            items[i].quality = items[i].quality - items[i].quality;
+                        }
+                    }
+                    else
+                    {
+                        if (items[i].quality < 50)
+                        {
+                            items[i].quality = items[i].quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+}
---
+++ GildedRose/src/GildedRose.Console/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRose.Console
+{
+    public class Item
+    {
+        public String name;
+
+        public int sellIn;
+
+        public int quality;
+
+        public Item(String name, int sellIn, int quality)
+        {
+            this.name = name;
+            this.sellIn = sellIn;
+            this.quality = quality;
+        }
+    }
+}
